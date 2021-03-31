69aa1be1bcf170cdd38d052871e279982a485d3d
Gabriel Silva <j.gabriel.888@gmail.com>
2021-03-31 23:46:00 +0000

    Select Injection sanitize

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,3 +1,20 @@
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = 'JUL02'
+// ORDER BY teacher;
+// `)
+//   .then(res => {
+//     console.log(res.rows)
+//     res.rows.forEach(teacher => {
+//       console.log(`${teacher.teacher} has made an assistance on ${teacher.cohort} cohort`);
+//     })
+//   });
+
+
 const {Pool} = require('pg');
 
 const pool = new Pool({
@@ -7,19 +24,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
-pool.query(`
+const queryString1 = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = 'JUL02'
-ORDER BY teacher;
-`)
+WHERE cohorts.name = $1
+ORDER BY teachers.name
+LIMIT $2;
+`;
+
+const cohortsName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`${cohortsName}`, limit];
+
+pool.query(queryString1, values)
   .then(res => {
-    console.log(res.rows)
-    res.rows.forEach(teacher => {
-      console.log(`${teacher.teacher} has made an assistance on ${teacher.cohort} cohort`);
-    })
-  });
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  }).catch(err => console.error('query error', err.stack));
